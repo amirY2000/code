d344d6c9dd82a50ee5864860077e8e30296b3144
Amir Yasaei <44714388+amirY2000@users.noreply.github.com>
2018-12-02 11:18:50 -0500

    some edits

--- checkers.py
+++ checkers.py
@@ -2,7 +2,6 @@ class Checkers:
 
     def __init__(self) -> list:
         self.board = [[" ",1,2,3,4,5,6,7,8],[1,"w",".","w",".","w",".","w","."],[2,".","w",".","w",".","w",".","w"],[3,"w",".","w",".","w",".","w","."],[4,".",0,".",0,".",0,".",0],[5,0,".",0,".",0,".",0,"."],[6,".","B",".","B",".","B",".","B"],[7,"B",".","B",".","B",".","B","."],[8,".","B",".","B",".","B",".","B"]]
-        
 
     def display_board(self) -> str:
         """
@@ -28,13 +27,8 @@ class Checkers:
         piece = input("which peice are you looking for ? ")
         a = piece[0]
         b = piece[1]
-        for i in range(100):
-            if str(i) == a:
-                a = i
-                for j in range(100):
-                    if str(j) == b:
-                        b = j
-                        break
+        a = int(a)
+        b = int(b)
         if self.board[b][a] == "w" or self.board[b][a] == "B":
             return True
         return False
@@ -54,34 +48,18 @@ class Checkers:
         """
         piece = input("Which piece would you like to move ? ")
         move = input("to?! ")
-        a = move[0]
-        b = move[1]
-        for i in range(100):
-            if str(i) == a:
-                a = i
-                for j in range(100):
-                    if str(j) == b:
-                        b = j
-                        break
-        if self.board[b][a] == 0:
-            g = piece[0]
-            h = piece[1]
-            for l in range(100):
-                if str(l) == g:
-                    g = l
-                    for p in range(100):
-                        if str(p) == h:
-                            h = p
-                            break
-            if self.board[h][g] == "w" or self.board[h][g] == "B":
-                if b-h == 1 or b-h == -1:
-                    if a-g == 1 or a-g == -1:
-                        return True
-                return False        
-            return False
+        a = int(move[0])
+        b = int(move[1])
+        g = int(piece[0])
+        print(self.board[h][g])
+        if self.board[h][g] == "w" or self.board[h][g] == "B":
+            if b-h == 1 or b-h == -1:
+                if a-g == 1 or a-g == -1:
+                    return True
+            return False        
         return False
                 
-    def update_board(self : list) -> bool:
+    def update_board(self : list)-> str:
         """
         Update board by moving piece to move. Return True if and only if
         the opposing player (the one that is not moving a piece) has no valid
@@ -90,32 +68,20 @@ class Checkers:
         >>>1
         >>>33
         >>>44
-        print board & True
+        print board
         """
         player = input("player 1 or player 2 ? ")
         if player != "1" and player != "2":
             return "please enter a valid player"
         piece = input("which piece do you want to move ? ")
         move = input("what is your destination ? ")
-        a = move[0]
-        b = move[1]
-        for i in range(100):
-            if str(i) == a:
-                a = i
-                for j in range(100):
-                    if str(j) == b:
-                        b = j
+        a = int(move[0])
+        b = int(move[1])
         if self.board[b][a] == 0:
-            g = piece[0]
-            h = piece[1]
-            for l in range(100):
-                if str(l) == g:
-                    g = l
-                    for p in range(100):
-                        if str(p) == h:
-                            h = p
-            if player == "1" and self.board[h][g] == "w":
-                if b-h == 1 and a-g == 1:
+            g = int(piece[0])
+            h = int(piece[1])
+            if player == "2" and self.board[h][g] == "B":
+                if b-h == -1 and a-g == 1 or a-g == -1:
                         temp = self.board[h][g]
                         self.board[h][g] = self.board[b][a]
                         self.board[b][a] = temp
@@ -123,23 +89,18 @@ class Checkers:
                             for c in d:
                                 print(c,end = " ")
                             print()
-            if player == "2" and self.board[h][g] == "B":
-                if b-h == -1 or a-g == -1:
+            if player == "1" and self.board[h][g] == "w":
+                if b-h == 1 and a-g == 1 or a-g == -1:
                     temp = self.board[h][g]
                     self.board[h][g] = self.board[b][a]
                     self.board[b][a] = temp
                     for d in self.board:
                         for c in d:
                             print(c,end = " ")
-                        print()
-        if self.valid_move() == True:
-            self.update_board()
-            return True
-        return False
+                        print()   
     def update_player(self,player) -> int:
         pass
         if __name__ == "__main__":
-            board = self.board
             player = 1
             gameover = False
             while not gameover:
@@ -150,7 +111,7 @@ class Checkers:
                 move = input("Player {}, where would you like to move the piece at {}?".format(player, piece))
                 while not self.valid_move():
                     move = input("Player {}, pick a valid move for the piece at {}.".format(player, piece))
-                gameover = self.update_board()
+                    gameover = self.update_board()
                 player = self.update_player(player)
             player = self.update_player(player)
             print("Game over, player {} wins!".format(player))
