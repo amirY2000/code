7534c27a59372bbe6f581eff11dfabe2230dd9f4
Amir Yasaei <44714388+amirY2000@users.noreply.github.com>
2018-12-01 20:26:39 -0500

    update_board added

--- checkers.py
+++ checkers.py
@@ -62,6 +62,7 @@ def valid_move(board) -> bool:
                 if str(j) == b:
                     b = j
                     break
+    print(board[b][a])
     if board[b][a] == 0:
         g = piece[0]
         h = piece[1]
@@ -89,13 +90,50 @@ def update_board(board : list) -> bool:
     the opposing player (the one that is not moving a piece) has no valid
     moves after updating the game.
     """
-    board = [["   ",1,2,3,4,5,6,7,8],[1,"|","w",".","w",".","w",".","w","."],[2,"|",".","w",".","w",".","w",".","w"],[3,"|","w",".","w",".","w",".","w","."],[4,"|",".",0,".",0,".",0,".",0],[5,"|",0,".",0,".",0,".",0,"."],[6,"|",".","B",".","B",".","B",".","B"],[7,"|","B",".","B",".","B",".","B","."],[8,"|",".","B",".","B",".","B",".","B"]]
+    board = [[" ",1,2,3,4,5,6,7,8],[1,"w",".","w",".","w",".","w","."],[2,".","w",".","w",".","w",".","w"],[3,"w",".","w",".","w",".","w","."],[4,".",0,".",0,".",0,".",0],[5,0,".",0,".",0,".",0,"."],[6,".","B",".","B",".","B",".","B"],[7,"B",".","B",".","B",".","B","."],[8,".","B",".","B",".","B",".","B"]]
     player = input("player 1 or player 2 ? ")
     piece = input("which piece do you want to move ? ")
     move = input("what is your destination ? ")
-    board = ['',"w",".","w",".","w",".","w",".",".","w",".","w",".","w",".","w","w",".","w",".","w",".","w",".",".",0,".",0,".",0,".",0,0,".",0,".",0,".",0,".",".","B",".","B",".","B",".","B","B",".","B",".","B",".","B",".",".","B",".","B",".","B",".","B"]
-    
-
+    a = move[0]
+    b = move[1]
+    for i in range(100):
+        if str(i) == a:
+            a = i
+            for j in range(100):
+                if str(j) == b:
+                    b = j
+    print(board[b][a])
+    if board[b][a] == 0:
+        g = piece[0]
+        h = piece[1]
+        for l in range(100):
+            if str(l) == g:
+                g = l
+                for p in range(100):
+                    if str(p) == h:
+                        h = p
+        if player == str(1) and board[h][g] == "w":
+            if b-h == 1 and a-g == 1:
+                    temp = board[h][g]
+                    board[h][g] = board[b][a]
+                    board[b][a] = temp
+                    for d in board:
+                        for c in d:
+                            print(c,end = " ")
+                        print()
+        if player == str(2) and board[h][g] == "B":
+            if b-h == -1 or a-g == -1:
+                temp = board[h][g]
+                board[h][g] = board[b][a]
+                board[b][a] = temp
+                for d in board:
+                    for c in d:
+                        print(c,end = " ")
+                    print()
+        else:
+            return "Please enter a valid player"
+board = initialize_board()
+print(update_board(board))
 def update_player(player) -> int:
     pass
     if __name__ == "__main__":
@@ -110,7 +148,7 @@ def update_player(player) -> int:
             move = input("Player {}, where would you like to move the piece at {}?".format(player, piece))
             while not valid_move(board):
                 move = input("Player {}, pick a valid move for the piece at {}.".format(player, piece))
-            gameover = update_board(board, move, piece, player)
+            gameover = update_board(board)
             player = update_player(player)
         player = update_player(player)
         print("Game over, player {} wins!".format(player))
