27b7f03dac70a9d883ec2f74165f7606d50fc3db
Amir Yasaei <44714388+amirY2000@users.noreply.github.com>
2019-02-26 14:04:52 -0500

    Add files via upload

--- mr.cheng.py
+++ mr.cheng.py
@@ -1,4 +1,4 @@
-'''
+
 def factor(a,b,c):
     """
     this func will get the a,b,c and give us the root of bionomial
@@ -25,8 +25,28 @@ def distribute(a,b,c,d):
     x = str((a*d)+(b*c))
     i = str(b*d)
     return z+"x^2"+"+"+" "+x+"x"+"+"+" "+i
+
+def advance_factor(a,b,c): 
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        ac4 = (4*a)*c
+        b2 = b**2
+        delta = b2-ac4
+        if delta < 0:
+            return " There isn't any real root"
+        elif delta == 0:
+            delta = delta**0.5
+            x = (-b+delta)/(2*a)
+            return x
+        else:
+            delta = delta**0.5
+            x1 = (-b+delta)/(2*a)
+            x2 = (-b-delta)/(2*a)
+            return x1 , x2
+
 def valid_choice(selection):
-    if selection == "f" or selection == "d":
+    if selection == "f" or selection == "d" or selection == "a":
         return True
     return False
 def valid_coef(a,b,c):
@@ -46,9 +66,17 @@ def valid_coef_d(a,b,c,d):
         return True
     except ValueError:
         return False
+def valid_coef_a(a,b,c):
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        return True
+    except ValueError:
+        return False
 
 if __name__ == "__main__":
-    selection = input("Would you like to (f)actor or (d)istribute? ")
+    selection = input("Would you like to (f)actor or (d)istribute? or (a) for advance factor? ")
     while valid_choice(selection) == False:
         selection = input("Select a valid option: 'f' for factor and 'd' for distribute :")
     if selection == "f":
@@ -68,23 +96,11 @@ if __name__ == "__main__":
             print(distribute(a,b,c,d))
         else:
             print("One of your coefficients is not valid !!")
-'''
-def advance_factor(a,b,c):
-    if type(a) != str and type(b) != str and type(c) != str: 
-        ac4 = (4*a)*c
-        b2 = b**2
-        delta = b2-ac4
-        if delta < 0:
-            return " There isn't any real root"
-        elif delta == 0:
-            delta = delta**0.5
-            x = (-b+delta)/(2*a)
-            return x
+    if selection == "a":
+        a = input("what is a ? ")
+        b = input("what is b ? ")
+        c = input("what is c ? ")
+        if valid_coef_a(a,b,c) == True:
+            print(advance_factor(a,b,c))
         else:
-            delta = delta**0.5
-            x1 = (-b+delta)/(2*a)
-            x2 = (-b-delta)/(2*a)
-            return x1 , x2
-    else:
-        return "One of your coefficients is not number!"
-print(advance_factor(1,2,1))
+            print("One of your coefficients is a string !!")
